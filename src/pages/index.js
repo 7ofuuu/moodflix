98dc47e11e0a7e69750f9ac39459a5d732b11cab
Mohammad Ilham Firdaus <ilhamfir09@gmail.com>
2025-06-29 23:59:21 +0700

    feat: cleaning code

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,5 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardContent, CardFooter } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/components/ui/select";
-import { Progress } from "@/components/ui/progress";
 import Head from "next/head";
 import Link from "next/link";
 
